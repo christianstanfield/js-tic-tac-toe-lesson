5739efa2a39c2a1ed7634d7685b9538f87a70812
Christian Stanfield <Ein@MacBook-Pro.local>
2015-02-27 19:26:51 -0600

    added comments

--- script.js
+++ script.js
@@ -1,58 +1,58 @@
 var players = ['X','O'];
 var turn = 0;
 
-var winningConditions = [[1,2,3],
+var winningConditions = [[1,2,3], // rows
                          [4,5,6],
                          [7,8,9],
-                         [1,4,7],
+                         [1,4,7], // columns
                          [2,5,8],
                          [3,6,9],
-                         [1,5,9],
+                         [1,5,9], // diagonals
                          [3,5,7]];
 
-$(function () {
+$(function () { // when the document is ready
 
-  $('td').on('click', function () {
-    if ($(this).text() === '') setValue(this);
+  $('td').on('click', function () { // when we click on a square
+    if ($(this).text() === '') setValue(this); // if it's empty set its value
   });
 });
 
 var setValue = function (square) {
 
-  $(square).text(players[turn]);
+  $(square).text(players[turn]); // set the square's value to the player whose turn it is
 
-  if (turn === players.length-1) {
+  if (turn === players.length-1) { // change each player's turn
     turn = 0;
   } else {
     turn += 1;
   }
-  
-  checkForWinner();
+
+  checkForWinner(); // check for a winner
 };
 
 var checkForWinner = function () {
 
-  for (var i = 0; i < winningConditions.length; i++) {
+  for (var i = 0; i < winningConditions.length; i++) { // for each set of win conditions
     var values = [];
     var positions = winningConditions[i];
-    for (var j = 0; j < positions.length; j++) {
-      values.push( $('#' + positions[j]).text() );
+    for (var j = 0; j < positions.length; j++) { // for each position of the current set
+      values.push( $('#' + positions[j]).text() ); // save that square's value
     }
 
-    if (winner(values)) displayWin(values);
+    if (winner(values)) displayWin(values); // check for a winner and display the result if one is found
   }
 };
 
 var winner = function (values) {
 
-  if (values.indexOf('') > -1) return false;
-  for (var i = 0; i < values.length; i++) {
-    if (i > 0 && values[i] != values[i-1]) return false;
+  if (values.indexOf('') > -1) return false; // if any squares are empty there's no winner in this set
+  for (var i = 0; i < values.length; i++) { // otherwise compare each value to see if they match
+    if (i > 0 && values[i] != values[i-1]) return false; // if they don't there's no winner
   }
-  return true;
+  return true; // if they all do a winner was found
 };
 
 var displayWin = function (values) {
-  $('h1').append('<br>' + values[0] + ' wins!');
-  $('td').unbind('click');
+  $('h1').append('<br>' + values[0] + ' wins!'); // display the player who won
+  $('td').unbind('click'); // make the squares no longer clickable
 };
