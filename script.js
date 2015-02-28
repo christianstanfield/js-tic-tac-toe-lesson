65fb453c40649ce06be0897856bc88643a9adfa4
Christian Stanfield <Ein@MacBook-Pro.local>
2015-02-27 19:09:46 -0600

    setup with td ids

--- script.js
+++ script.js
@@ -1,40 +1,52 @@
+var players = ['X','O'];
+var turn = 0;
+
+var winningConditions = [[1,2,3],
+                         [4,5,6],
+                         [7,8,9],
+                         [1,4,7],
+                         [2,5,8],
+                         [3,6,9],
+                         [1,5,9],
+                         [3,5,7]];
+
 $(function () {
-  var players = ['X','O'];
-  var turn = 0;
 
   $('td').on('click', function () {
-    $(this).text(players[turn]);
-    if (turn === players.length-1) {
-      turn = 0;
-    } else {
-      turn += 1;
-    }
-    checkForWinner();
+    if ($(this).text() === '') setValue(this);
   });
 });
 
-var checkForWinner = function () {
+var setValue = function (square) {
 
-  checkRows();
+  $(square).text(players[turn]);
 
+  if (turn === players.length-1) {
+    turn = 0;
+  } else {
+    turn += 1;
+  }
+  
+  checkForWinner();
 };
 
-var checkRows = function () {
+var checkForWinner = function () {
 
-  $('tr').each(function () {
+  for (var i = 0; i < winningConditions.length; i++) {
     var values = [];
-    $(this).find('td').each(function () {
-      var value = $(this).text();
-      values.push(value);
-    });
+    var positions = winningConditions[i];
+    for (var j = 0; j < positions.length; j++) {
+      values.push( $('#' + positions[j]).text() );
+    }
+
     if (winner(values)) displayWin(values);
-  });
+  }
 };
 
 var winner = function (values) {
 
   if (values.indexOf('') > -1) return false;
-  for (i = 0; i < values.length; i++) {
+  for (var i = 0; i < values.length; i++) {
     if (i > 0 && values[i] != values[i-1]) return false;
   }
   return true;
