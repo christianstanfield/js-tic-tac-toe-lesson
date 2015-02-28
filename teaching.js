6c3616b43d5b3effcf55263d19a49d679427cda5
Christian Stanfield <Ein@MacBook-Pro.local>
2015-02-27 20:16:23 -0600

    added teaching files

---
+++ teaching.js
@@ -0,0 +1,24 @@
+// Concepts:
+
+// 1. Variables
+var one = 1;
+
+// 2. Arrays
+[1,2,3];
+
+// 3. Functions
+function (parameter) {
+  // code block
+}
+
+// 4. If statements
+if (1 + 1 === 2) {
+  // if true do this
+} else {
+  // otherwise do this
+}
+
+// 5. For loops
+for (var i = 0; i < array.length; i++) {
+  array[i]
+}
